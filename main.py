9569cb789e736af472c55056ec09e764fe0bdf34
luize Baldoni <oliveira.luize@acad.ufsm.br>
2022-09-22 14:59:38 -0300

    adicionou inicio de layout e pasta de assets

--- main.py
+++ main.py
@@ -2,31 +2,52 @@ from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
 import numpy as np
 
+
 def define_layout():
     app = Dash(__name__)
 
     app.layout = html.Div([
-        html.H1("Site topzera"),
-        html.P("Sério. Esse site é muito top"),
+
+        html.Title('Math Visualization'),
+        html.Link(href='https://fonts.googleapis.com/css?family=Rubik Dirt', rel='stylesheet'),
+        html.Header([
+            html.Div(
+                className="app-main",
+                children=[
+                    html.Div(className="app-main--title")
+                ]
+            ),
+         html.Div([
+            html.P(["Math Visualization"], className="titulo"),
+            html.Section(),
+         ], className="container"),
+         ]),
+         html.Br(),
+        html.Br(),
         html.Label("Digite uma equação de reta: ", htmlFor="input_equacao_reta"),
+        html.Br(),
+        html.Br(),
         dcc.Input(
             id="input_equacao_reta",
             placeholder='',
             type='text',
             value=''
         ),
+        html.Br(),
+        html.Br(),
         dcc.RadioItems(
             id="seletor_intersecao_reta_circunferencia",
-            options=["Secante", "Tangente", "Disjuntas"],
+            options=["Secante", "Tangente","Disjuntas"],
             value="Secante",
         ),
+        html.Br(),
         dcc.Loading(dcc.Graph(id="grafico"), type="cube"),
-    ])
+    ], className="body")
 
     return app
 
-def main():
 
+def main():
     app = define_layout()
 
     @app.callback(
@@ -37,7 +58,7 @@ def main():
         y = np.arange(10)
 
         fig = go.Figure(go.Scatter(
-            x=x, 
+            x=x,
             y=y,
             name='função linear',
             showlegend=True
