482a4571ae09e55dac6e865b96987a4ed5b2d61e
Aline <alinecominrighi@gmail.com>
2022-09-24 11:15:06 -0300

    parte que antecede y

--- funcoes/geral_para_reduzida.py
+++ funcoes/geral_para_reduzida.py
@@ -9,15 +9,23 @@ def geral_para_reduzida(geral: str) -> str:
 
     parteImportante = str(geral.split('=')[0])
 
-    if 'y' in parteImportante:
-        print('equação correta')
-    else:
-        print('equação errada')
+    try:
+        if 'y' in parteImportante:
+            antessede_y = (parteImportante.split("y")[0])
+            print(antessede_y)
+
+        else:
+            print('não tem y')
+
+    except:
+        raise Exception("Errado, não tem y")
+
+    return ...
 
 
 def main():
-    geral = '2X - 3y-3=0'
-    print(geral_para_reduzida('-3 X+3=0'))
+    geral = '2y- 3x-3=0'
+    print(geral_para_reduzida('-3x+5y+3=0'))
     print(geral_para_reduzida(geral))
 
 
