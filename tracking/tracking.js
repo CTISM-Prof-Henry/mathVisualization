6b8e53ac8841a98c4da1b653e6e6f9032264a8e1
henryzord <henry.cagnini@gmail.com>
2022-09-26 17:26:31 -0300

    added timer

--- tracking/tracking.js
+++ tracking/tracking.js
@@ -51,38 +51,87 @@ let finish_session_modal = document.getElementById("finish_session_modal");
 // botões que abrem os modais
 let button_finish_session = document.getElementById("button_finish_session");
 let button_start_session = document.getElementById("button_start_session");
+let close_finish_session_modal = document.getElementById("close_finish_session_modal");
 
 // spans que fecham os modais (botão x)
 let span_start_session_modal = document.getElementById("span_start_session_modal");
 let span_finish_session_modal = document.getElementById("span_finish_session_modal");
 
-// começa com o modal de instruções/início da sessão aberto
-span_start_session_modal.style.display = "block";
-start_session_modal.style.display = "block";
+let timer = 10000; // milisegundos
+let start = new Date().getTime();
+let running = false;
 
-// quando o usuário clicar no botão de finalizar sessão, abre modal com resultados
-button_finish_session.onclick = function() {
-  finish_session_modal.style.display = "block";
+let countdown_timer = document.getElementById("countdown_timer");
+
+function update_countdown_timer() {
+    let seconds_timer;
+    if(running) {
+         seconds_timer = (timer - ((new Date().getTime()) - start)) / 1000;
+    } else {
+        seconds_timer = timer/1000;
+    }
+    let minutes = parseInt(seconds_timer / 60);
+    let seconds = parseInt(seconds_timer % 60);
+
+    if((minutes.toString().length) === 1) {
+        minutes = '0' + minutes;
+    }
+    if(seconds.toString().length === 1) {
+        seconds = '0' + seconds;
+    }
+
+    countdown_timer.textContent = "Tempo restante: " + minutes + ":" + seconds;
 }
 
+function start_session_func_show() {
+    span_start_session_modal.style.display = "block";
+    start_session_modal.style.display = "block";
+}
+
+function finish_session_func_show() {
+    finish_session_modal.style.display = "block";
+    span_finish_session_modal.style.display = "block";
+
+    running = false;
+}
+function start_session_func_close() {
+    span_start_session_modal.style.display = "none";
+    start_session_modal.style.display = "none";
+
+    start = new Date().getTime();
+    running = true;
+
+    setTimeout(finish_session_func_show, timer);
+}
+
+function finish_session_func_close() {
+    span_finish_session_modal.style.display = "none";
+    finish_session_modal.style.display = "none";
+}
+
+function restart_session_func() {
+    finish_session_func_close();
+    start_session_func_show();
+}
+
+// começa com o modal de instruções/início da sessão aberto
+start_session_func_show();
+window.setInterval(update_countdown_timer, 1000);  // a cada segundo
+
+// quando o usuário apertar o x do span dentro do modal, fecha-o
+span_start_session_modal.onclick = start_session_func_close;
 // quando o usuário clicar no botão de iniciar sessão (dentro do modal),
 // fecha o modal
-button_start_session.onclick = function() {
-  span_start_session_modal.style.display = "none";
-  start_session_modal.style.display = "none";
-}
+button_start_session.onclick = start_session_func_close;
 
-// quando o usuário apertar o x do span dentro do modal, fecha-o
-span_start_session_modal.onclick = function() {
-  span_start_session_modal.style.display = "none";
-  start_session_modal.style.display = "none";
-}
+// quando o usuário clicar no botão de finalizar sessão, abre modal com resultados
+button_finish_session.onclick = finish_session_func_show;
 
+// quando o usuário apertar o botão de fechar dentro do modal, fecha-o
+close_finish_session_modal.onclick = restart_session_func;
 // quando o usuário apertar o x do span dentro do modal, fecha-o
-span_finish_session_modal.onclick = function() {
-  span_finish_session_modal.style.display = "none";
-  finish_session_modal.style.display = "none";
-}
+span_finish_session_modal.onclick = restart_session_func;
+
 
 // When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
