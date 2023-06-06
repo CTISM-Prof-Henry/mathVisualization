2b0def4d33bed80a6189858e37197e4847b0d9da
henryzord <henry.cagnini@gmail.com>
2023-06-06 07:45:27 -0300

    improved ploting

--- analysis/compute_tests_statistics_v3.py
+++ analysis/compute_tests_statistics_v3.py
@@ -9,25 +9,24 @@ import os
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from matplotlib import patheffects as path_effects
-
-from compute_tests_statistics import to_frequencies, plot_heatmap
 
 
-def process():
-    pass
-
-def plot_local(pre_questions, equis, pre, post, nrows, ncols, title):
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
-    axes = np.ravel(axes)
-
-    for i, p in enumerate(pre_questions):
+def plot_local(pre_questions, equis, pre, post, axes, count, title):
+    x_ticks = np.arange(2)
+    for p in pre_questions:
         q = equis.loc[equis['pre-test'] == p]['post-test'].values.tolist()
 
-        axes[i].bar(np.arange(2), [pre[p].mean(), post[q].mean(axis=0).mean()])
-        axes[i].set_title(f'{p}')
-        fig.suptitle(title)
-        fig.tight_layout()
+        if len(q) > 0:
+            x_ticklabels = [p, '\n'.join(q)]
+
+            axes[count].bar(x_ticks, [pre[p].mean(), post[q].mean(axis=0).mean()])
+            axes[count].set_yticks([0, 1])
+            axes[count].set_xticks(x_ticks)
+            axes[count].set_xticklabels(x_ticklabels)
+            axes[count].set_title(f'{title}, {p}')
+            count += 1
+
+    return count
 
 
 def main():
@@ -36,20 +35,21 @@ def main():
     equis = pd.read_csv(os.path.join('answers', 'raw', 'equivalencies_simplified.csv'))
 
     turmas = ['eletro', 'info', 'mec']
-    pre_questions = pre.columns.unique()
+    pre_questions = pre.columns.unique().sort_values()
 
-    nrows = len(pre_questions) // 2
-    ncols = len(pre_questions) // nrows
-
-    plot_local(pre_questions, equis, pre, post, nrows, ncols, 'todas as turmas')
-    plt.show()
+    count = 0
+    fig, axes = plt.subplots(nrows=5, ncols=4)
+    axes = np.ravel(axes.T)
+    count = plot_local(pre_questions, equis, pre, post, axes, count, 'todas as turmas')
 
     for turma in turmas:
         pre_this_class = pre.loc[turma, slice(None)]
         post_this_class = post.loc[turma, slice(None)]
 
-        plot_local(pre_questions, equis, pre_this_class, post_this_class, nrows, ncols, f'turma {turma}')
+        count = plot_local(pre_questions, equis, pre_this_class, post_this_class, axes, count, f'turma {turma}')
 
+    fig.suptitle('Taxa de acertos entre questões do pré-questionário e suas equivalências no pós-questionário')
+    fig.tight_layout()
     plt.show()
 
 
