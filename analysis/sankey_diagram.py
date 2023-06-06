e9ad2419b3e98b22d387005f61e6c0211a51e2b4
henryzord <henry.cagnini@gmail.com>
2023-06-06 16:03:49 -0300

    improved readability

--- analysis/sankey_diagram.py
+++ analysis/sankey_diagram.py
@@ -11,8 +11,8 @@ def main():
     pd.options.display.max_rows = 8
     df = pd.read_csv(os.path.join('answers', 'raw', 'equivalencies_simplified.csv'))
 
-    labels_pre = np.sort(df['pre-test'].unique()).tolist()  # type: list
-    labels_post = np.sort(df['post-test'].unique()).tolist()  # type: list
+    labels_pre = [f"Questão {x.split('.')[-1]}" for x in np.sort(df['pre-test'].unique()).tolist()]  # type: list
+    labels_post = [f"Questão {x.split('.')[-1]}" for x in np.sort(df['post-test'].unique()).tolist()]  # type: list
 
     colors_pre = {k: to_hex(cm.viridis(i)) for k, i in zip(labels_pre, np.linspace(0, 1, len(labels_pre)))}
     colors_post = {k: to_hex(cm.viridis(i)) for k, i in zip(labels_post, np.linspace(0, 1, len(labels_post)))}
@@ -21,7 +21,8 @@ def main():
     colors_dict = colors_pre
 
     sankey.sankey(
-        df['pre-test'], df['post-test'],
+        df['pre-test'].apply(lambda x: f"Questão {x.split('.')[-1]}"),
+        df['post-test'].apply(lambda x: f"Questão {x.split('.')[-1]}"),
         aspect=20,
         # leftLabels=labels_post,
         # rightLabels=labels_pre,
